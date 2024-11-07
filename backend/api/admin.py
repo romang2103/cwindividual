eeb5f51554425a8706e2e2b0562a742cd4a9339a
Roman <romang2103@gmail.com>
2024-11-07 20:01:46 +0000

    first

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Line, LineStation, Station
+
+admin.site.register(Station)
+admin.site.register(Line)
+admin.site.register(LineStation)
+
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+    
+class Station(models.Model):
+    name = models.CharField(max_length=128)
+    
+    def __str__(self):
+        return self.name
+    
+class Line(models.Model):
+    name = models.CharField(max_length=128)
+    stations = models.ManyToManyField(Station, through="LineStation")
+    number_of_stations = models.IntegerField()
+    available_on_weekend = models.BooleanField(default=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
 
-# Create your models here.
+class LineStation(models.Model):
+    station = models.ForeignKey(Station, on_delete=models.CASCADE)
+    line = models.ForeignKey(Line, on_delete=models.CASCADE)
+    order = models.PositiveIntegerField()
+    notes = models.TextField(blank=True)
+    
+    
+    def __str__(self):
+        return f"{self.line} - {self.station} - {self.order}"
